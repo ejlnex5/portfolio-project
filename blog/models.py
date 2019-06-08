06448b9eefbca191767953520e1fa18993640b9f
Your Name <you@example.com>
2019-06-08 16:48:52 -0700

    Updated jobs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create blog models here.
+    #title
+    #pubdate
+    #body
+    #image
+
+class Blog(models.Model):
+    title    = models.CharField(max_length=140)
+    pub_date = models.DateTimeField()
+    body     = models.TextField()
+    image    = models.ImageField(upload_to='images/')
+
+# Migrate
+
+# Add to admin
